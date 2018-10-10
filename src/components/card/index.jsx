498439cc478aade4cb2c18d59c96559babd30a5f
lijiahao8898 <472239548@qq.com>
2018-10-10 15:06:14 +0800

    增加sass

---
+++ src/components/card/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './index.scss';
+
+class Card extends Component {
+    render () {
+        return (
+            <div id="card">
+
+            </div>
+        )
+    }
+}
